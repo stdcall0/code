6afe52df4c2f820a3ddf456b813df721d2de87cf
ImChinaNB <hanghang0702@hotmail.com>
2021-04-16 18:38:17 +0800

    template: template_simp (without using C++11)

---
+++ templates/template_simp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define Rn(a, n) a + 1, a + n + 1
+typedef long long ll;
+typedef unsigned long long ull;
+inline ll R() {
+  ll ret(0), c(0); bool neg(0); while (!isdigit(c = std::getchar())) neg = c == '-';
+  ret = c - '0'; while (isdigit(c = std::getchar())) ret *= 10ll, ret += c - '0';
+  return neg ? -ret : ret;
+}
+
+const int N = 1e5 + 5;
+
+int main() {
+  
+  return 0;
+}
