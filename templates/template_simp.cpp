7027700a8d8213f331745507564333cc92fcb2ab
ImChinaNB <hanghang0702@hotmail.com>
2021-04-29 19:19:29 +0800

    template(template_simp): shorten code

--- templates/template_simp.cpp
+++ templates/template_simp.cpp
@@ -4,16 +4,8 @@
 #define Edge(T, x, y) T[x].push_back(y); T[y].push_back(x) //; printf("Edge #%s: %d - %d\n", #T, x, y)
 typedef long long ll;
 typedef unsigned long long ull;
-inline ll Rl() {
-  ll ret(0); int c(0); bool neg(0); while (!isdigit(c = std::getchar())) neg = c == '-';
-  ret = c - '0'; while (isdigit(c = std::getchar())) ret *= 10ll, ret += c - '0';
-  return neg ? -ret : ret;
-}
-inline int R() {
-  int ret(0); int c(0); bool neg(0); while (!isdigit(c = std::getchar())) neg = c == '-';
-  ret = c - '0'; while (isdigit(c = std::getchar())) ret *= 10ll, ret += c - '0';
-  return neg ? -ret : ret;
-}
+inline ll Rl() { ll ret(0); int c(0); bool neg(0); while (!isdigit(c = std::getchar())) neg = c == '-'; ret = c - '0'; while (isdigit(c = std::getchar())) ret *= 10ll, ret += c - '0'; return neg ? -ret : ret; }
+inline int R() { int ret(0), c(0); bool neg(0); while (!isdigit(c = std::getchar())) neg = c == '-'; ret = c - '0'; while (isdigit(c = std::getchar())) ret *= 10ll, ret += c - '0'; return neg ? -ret : ret; }
 
 const int N = 1e5 + 5;
 
