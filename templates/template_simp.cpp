6db854db1ab36ac60f9c573d00b5c0f802560ced
ImChinaNB <hanghang0702@hotmail.com>
2021-05-11 20:53:59 +0800

    template(template_simp): shorter code

--- templates/template_simp.cpp
+++ templates/template_simp.cpp
@@ -1,11 +1,8 @@
-#include <bits/stdc++.h>
-
-#define Rn(a, n) a + 1, a + n + 1
-#define Edge(T, x, y) T[x].push_back(y); T[y].push_back(x) //; printf("Edge #%s: %d - %d\n", #T, x, y)
-typedef long long ll;
-typedef unsigned long long ull;
-inline ll Rl() { ll ret(0); int c(0); bool neg(0); while (!isdigit(c = std::getchar())) neg = c == '-'; ret = c - '0'; while (isdigit(c = std::getchar())) ret *= 10ll, ret += c - '0'; return neg ? -ret : ret; }
-inline int R() { int ret(0), c(0); bool neg(0); while (!isdigit(c = std::getchar())) neg = c == '-'; ret = c - '0'; while (isdigit(c = std::getchar())) ret *= 10ll, ret += c - '0'; return neg ? -ret : ret; }
+#include<bits/stdc++.h>
+#define Rn(a,n)a+1,a+n+1
+#define Edge(T,x,y)T[x].push_back(y);T[y].push_back(x)
+#define rep(i,l,r)for(int i=(l),_r_R=(r);i<=_r_R;++i)
+typedef long long ll;typedef long double ld;typedef unsigned long long ull;inline ll Rl(){ll ret(0);int c(0);bool neg(0);while(!isdigit(c=std::getchar()))neg=c=='-';ret=c-'0';while(isdigit(c=std::getchar()))ret*=10ll,ret+=c-'0';return neg?-ret:ret;}inline int R(){int ret(0),c(0);bool neg(0);while(!isdigit(c=std::getchar()))neg=c=='-';ret=c-'0';while(isdigit(c=std::getchar()))ret*=10ll,ret+=c-'0';return neg?-ret:ret;}
 
 const int N = 1e5 + 5;
 
