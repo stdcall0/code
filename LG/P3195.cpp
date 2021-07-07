b481a67f815a844028ae7897770e1ded24ec71a8
ImChinaNB <hanghang0702@hotmail.com>
2021-07-07 09:11:33 +0800

    sync: school

--- LG/P3195.cpp
+++ LG/P3195.cpp
@@ -4,39 +4,33 @@
 #define rep(i,l,r)for(int i=(l),_r_R=(r);i<=_r_R;++i)
 typedef long long ll;typedef long double ld;typedef unsigned long long ull;inline ll Rl(){ll ret(0);int c(0);bool neg(0);while(!isdigit(c=std::getchar()))neg=c=='-';ret=c-'0';while(isdigit(c=std::getchar()))ret*=10ll,ret+=c-'0';return neg?-ret:ret;}inline int R(){int ret(0),c(0);bool neg(0);while(!isdigit(c=std::getchar()))neg=c=='-';ret=c-'0';while(isdigit(c=std::getchar()))ret*=10ll,ret+=c-'0';return neg?-ret:ret;}
 
-const int N = 3e5 + 5;
-int n;
-ll s, t[N], c[N], f[N];
+const int N = 5e4 + 5;
+int n, L;
+ll c[N], f[N];
 
 std::deque<int> Q;
-inline ll dX(int i, int j) { return c[j] - c[i]; }
-inline ll dY(int i, int j) { return f[j] - f[i] - c[j] * s + c[i] * s; }
+auto sqr = [](auto x) { return x * x; };
+inline ll X(int i) { return c[i]; } 
+inline ll Y(int i) { return f[i] + sqr(c[i]); }
+inline ll K(int i) { return (c[i] - L) << 1ll; }
+inline ll B(int i) { return f[i] - sqr(c[i] - L); }
+
+inline ll dX(int i, int j) { return X(j) - X(i); }; inline ll dY(int i, int j) { return Y(j) - Y(i); }
 #define head Q[0], Q[1]
 #define tail Q[Q.size() - 2], Q[Q.size() - 1]
-
-inline size_t find(int i) {
-  size_t l = 0, r = Q.size() - 1, ret = Q.size() - 1;
-  while (l <= r) {
-    static size_t mid;
-    mid = (l + r) >> 1;
-    if (dY(Q[mid], Q[mid+1]) > t[i] * dX(Q[mid], Q[mid+1])) {
-      ret = mid; r = mid - 1;
-    } else l = mid + 1;
-  }
-  return ret;
-}
+#define now Q[Q.size() - 1], i
 
 int main() {
-  n = R(), s = Rl();
+  n = R(), L = R() + 1;
   rep(i, 1, n) {
-    t[i] = t[i - 1] + Rl();
-    c[i] = c[i - 1] + Rl();
+    c[i] = c[i - 1] + R();
   }
-  Q.push_back(0);
+  rep(i, 1, n) c[i] = c[i] + i;
+  f[0] = 0; Q.push_back(0);
   rep(i, 1, n) {
-    int p = Q[find(i)];
-    f[i] = f[p] + t[i] * (c[i] - c[p]) + s * (c[n] - c[p]);
-    while (Q.size() > 1 && dY(tail) * dX(Q[Q.size() - 1], i) >= dY(Q[Q.size() - 1], i) * dX(tail)) Q.pop_back();
+    while (Q.size() > 1 && K(i) * dX(head) >= dY(head)) Q.pop_front();
+    f[i] = f[Q[0]] + sqr(c[i] - c[Q[0]] - L);
+    while (Q.size() > 1 && dY(tail) * dX(now) >= dY(now) * dX(tail)) Q.pop_back();
     Q.push_back(i);
   }
   printf("%lld\n", f[n]);
